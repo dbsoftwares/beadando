2f635f492eb5081ae88b56aae41ee96f8fbe3f92
Péter György Tóth <tothpetergyorgy@gmail.com>
2012-12-13 23:14:05 +0100

    bugfix + modify the wirtefile to the requirements

--- beadando/src/org/dbsoftwares/beadando/Student.java
+++ beadando/src/org/dbsoftwares/beadando/Student.java
@@ -17,17 +17,17 @@ public class Student {
     String thirdScore;
     String fourthScore;
     String fifthScore;
-    int avgScore;
+    int sumScore;
 
-    protected int calculateAvgScore() {
+    protected int calculateSumScore() {
         int firstScoreNum = Integer.parseInt(firstScore);
         int secondScoreNum = Integer.parseInt(secondScore);
         int thirdScoreNum = Integer.parseInt(thirdScore);
         int fourthScoreNum = Integer.parseInt(fourthScore);
         int fifthScoreNum = Integer.parseInt(fifthScore);
 
-        this.avgScore = Math.round((firstScoreNum + secondScoreNum + thirdScoreNum
-                + fourthScoreNum + fifthScoreNum) / 5);
-        return avgScore;
+        this.sumScore = firstScoreNum + secondScoreNum + thirdScoreNum
+                + fourthScoreNum + fifthScoreNum;
+        return sumScore;
     }
 }
--- beadando/src/org/dbsoftwares/beadando/TaskClass.java
+++ beadando/src/org/dbsoftwares/beadando/TaskClass.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Scanner;
 
 /**
@@ -25,8 +26,8 @@ public class TaskClass {
         int i = 0;
         try {
             File fileToRead = new File(filePath);
-            System.out.println(fileToRead);
             Scanner inputFile = new Scanner(fileToRead);
+            System.out.println(inputFile.hasNextLine());
             while (inputFile.hasNextLine()) {
                 /**
                  * create a new student object
@@ -40,6 +41,8 @@ public class TaskClass {
                 students[i].thirdScore = inputFile.nextLine();
                 students[i].fourthScore = inputFile.nextLine();
                 students[i].fifthScore = inputFile.nextLine();
+                students[i].calculateSumScore();
+                System.out.println(i);
                 i++;
             }
             inputFile.close();
@@ -47,9 +50,12 @@ public class TaskClass {
             System.err.println("File not found!");
         }
         
-        System.out.println(students[0].name);
-        System.out.println(students[0].city); 
-        System.out.println(students[0].calculateAvgScore());
+        for (i = 0; i == students.length; i++) {
+            if (students[i].sumScore >= 40){
+                // got the 40%
+                System.out.println(students[i].name+" "+students[i].city);
+            }
+        }
     }
 
     public void executeFileWrite(String filePath) {
@@ -58,28 +64,11 @@ public class TaskClass {
             FileWriter fstream = new FileWriter(fileToWrite);
             BufferedWriter out = new BufferedWriter(fstream);
             
-            out.append(sBuilder.toString());
+            out.append("this file has been created @ "+Calendar.getInstance().getTime()+"!");
             out.flush();
             out.close();
         } catch (IOException ex) {
             System.err.println("File not found!");
         }
-    }
-    
-    private void printSuccessedStudents(Student[] students){
-        final int maxScore = 100;
-        
-        for (int i = 0; i <= students.length; i++) {
-            if (students[i].avgScore > 40){
-                // megvan a 40%, kiírom a nevét, meg a várost
-                System.out.println(students[i].name+" "+students[i].city);
-                
-                // hozzárakom a stringBuilderhez, amit majd kiírok a fájlba
-                sBuilder.append(students[i].name+" "+students[i].city+"\n");
-            }
-        }
- 
-                
-        
     }
 }
